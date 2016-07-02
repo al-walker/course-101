fd213b7c9394f6cf5610bac0933b1221abcc2f04
AWW <walker.albert@gmail.com>
2016-07-02 18:47:36 -0400

    Remove test_method from rock_paper_scissors.rb

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,16 +1,8 @@
 VALID_CHOICES = %w(rock paper scissors)
 
-def test_method
-  prompt('test message')
-end
-
-#test_method           # second, try invoking "test_method" here
-
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
-
-test_method           # first, try invoking "test_method" here
 
 def display_results(player, computer)
   if (player == 'rock' && computer == 'scissors') ||
