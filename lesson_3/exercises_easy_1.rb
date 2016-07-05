96789fd1bdedbca44c47e0bfe49bef7e8f64f773
AWW <walker.albert@gmail.com>
2016-07-05 18:55:16 -0400

    Add exercises_easy_1.rb - up to Question 9

---
+++ lesson_3/exercises_easy_1.rb
@@ -0,0 +1,81 @@
+# Question 1
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+# It prints out 1, 2, 3, 4 as strings (puts calls to_s on its argument)
+# numbers.uniq returns a new Array object with unique elements. It does not modify the array (.uniq! is the destructive version)
+
+# Question 2
+# Describe the difference between ! and ? in Ruby. And explain what would happen in the following scenarios:
+# ! Not
+# ? : ternary operator for if...else
+
+# what is != and where should you use it?
+# Not equals - conditional expression
+
+# put ! before something, like !user_name
+# turn any object to the opposite of its boolean equivalent.
+
+# put ! after something, like words.uniq!
+# may signal the method is destructive - Mutates the caller
+# part of the method name - depends on implementation
+
+# put ? before something
+# ternary operator
+
+# put ? after something
+# part of the method name
+
+# put !! before something, like !!user_name
+# turn any object into its boolean equivalent
+
+#Question 3 - Replace the word "important" with "urgent" in this string:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.gsub!('important', 'urgent')
+
+p advice
+
+# Question 4
+
+numbers = [1, 2, 3, 4, 5]
+p numbers.delete_at(1) # deletes 2
+
+numbers = [1, 2, 3, 4, 5]
+p numbers.delete(1) # deletes 1
+
+# Question 5
+
+p (10..100).include?(42)
+
+# Question 6
+
+famous_words = "seven years ago..."
+p famous_words.prepend("Four score and ")
+
+famous_words = "seven years ago..."
+p "Four score and " + famous_words
+
+# Question 7
+
+def add_eight(number)
+  number + 8
+end
+
+number = 2
+
+how_deep = "number"
+5.times { how_deep.gsub!("number", "add_eight(number)") }
+
+p how_deep
+p eval(how_deep)
+
+# Question 9
+
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barney", "Betty"]
+p flintstones << ["BamBam", "Pebbles"]
+p flintstones.flatten!
