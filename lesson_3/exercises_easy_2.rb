dabc7ee47b37e0ff4f2c7559e233b28bcb5dd99f
AWW <walker.albert@gmail.com>
2016-07-07 15:02:45 -0400

    Complete exercises_easy_2.rb

--- lesson_3/exercises_easy_2.rb
+++ lesson_3/exercises_easy_2.rb
@@ -40,3 +40,50 @@ p munsters_description.upcase
 # "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
 # "the munsters are creepy in a good way."
 # "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+
+# Question 5
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+p ages.merge!(additional_ages)
+
+# Question 6
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+p ages.values.min
+# or
+minimum = ages.values.max
+ages.each do |_, v|
+  if v < minimum
+    minimum = v
+  end
+end
+p minimum
+
+# Question 7
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+p advice.include?('Dino')
+# or
+p advice.match("dino")
+
+# Question 8
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones.index { |name| name.start_with?('Be') }
+# or
+p flintstones.index { |name| name[0, 2] == "Be" }
+
+# Question 9
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones.map! { |name| name[0..2] }
+
+# Question 10
+
+p flintstones.map! { |name| name[0,3] }
