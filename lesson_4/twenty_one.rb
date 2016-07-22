8c4c3b8c07bdb715e29abab133caef8bbcce358a
AWW <walker.albert@gmail.com>
2016-07-22 09:01:27 -0400

    Rubocop started revision

--- lesson_4/twenty_one.rb
+++ lesson_4/twenty_one.rb
@@ -5,16 +5,16 @@ dealer_hand = []
 
 # Initialize Deck
 cards =
-[['H', '2'], ['H', '3'],['H', '4'],['H', '5'],['H', '6'],
-['H', '7'], ['H', '8'], ['H', '9'], ['H', '10'], ['H', 'J'],
-['H', 'Q'], ['H', 'K'], ['H', 'A'], ['S', '2'], ['S', '3'],
-['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'],
-['S', '10'], ['S', 'J'], ['S', 'Q'], ['S', 'K'], ['S', 'A'],
-['D', '2'], ['D', '3'], ['D', '4'], ['D', '5'], ['D', '6'],
-['D', '7'], ['D', '8'], ['D', '9'], ['D', '10'], ['D', 'J'], ['D', 'Q'],
-['D', 'K'], ['D', 'A'], ['C', '2'], ['C', '3'], ['C', '4'],
-['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '10'],
-['C', 'J'], ['C', 'Q'], ['C', 'K'], ['C', 'A']]
+ [['H', '2'], ['H', '3'],['H', '4'], ['H', '5'], ['H', '6'],
+  ['H', '7'], ['H', '8'], ['H', '9'], ['H', '10'], ['H', 'J'],
+  ['H', 'Q'], ['H', 'K'], ['H', 'A'], ['S', '2'], ['S', '3'],
+  ['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'],
+  ['S', '10'], ['S', 'J'], ['S', 'Q'], ['S', 'K'], ['S', 'A'],
+  ['D', '2'], ['D', '3'], ['D', '4'], ['D', '5'], ['D', '6'],
+  ['D', '7'], ['D', '8'], ['D', '9'], ['D', '10'], ['D', 'J'], ['D', 'Q'],
+  ['D', 'K'], ['D', 'A'], ['C', '2'], ['C', '3'], ['C', '4'],
+  ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '10'],
+  ['C', 'J'], ['C', 'Q'], ['C', 'K'], ['C', 'A']]
 
 # Deal cards to player and dealer
 
@@ -38,7 +38,7 @@ def total(hand)
   end
 
   # correct for Aces
-  values.select { |value| value == "A"}.count.times do
+  values.select { |value| value == "A" }.count.times do
     sum -= 10 if sum > 21
   end
 
@@ -52,18 +52,18 @@ end
 # Player turn: hit or stay
 # - repeat until bust or "stay"
 def player_turn(cards, player_hand)
-  puts "#{player_hand}"
+  p player_hand.to_s
   answer = nil
 
   loop do
     puts "hit or stay?"
     answer = gets.chomp
     if answer == 'hit'
-    player_hand << cards.slice!(rand(0..cards.size - 1))
-    puts "#{player_hand}"
-  else
-    puts 'player stays'
-  end
+      player_hand << cards.slice!(rand(0..cards.size - 1))
+      p player_hand.to_s
+    else
+      puts 'player stays'
+    end
     break if answer == 'stay' || busted?(player_hand)
   end
 
