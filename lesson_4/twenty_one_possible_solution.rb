81ee3f5b7151952725b0a64df239e03ffb7df3c1
AWW <walker.albert@gmail.com>
2016-07-24 09:02:06 -0400

    Review possible solution - Using Pry

---
+++ lesson_4/twenty_one_possible_solution.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def initialize_deck
+  SUITS.product(VALUES).shuffle
+end
