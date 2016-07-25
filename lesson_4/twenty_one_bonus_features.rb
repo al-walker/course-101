debed6b0f07bf3a72704b3088e966ea06ad1cd98
AWW <walker.albert@gmail.com>
2016-07-25 14:06:56 -0400

    Start bonus features

---
+++ lesson_4/twenty_one_bonus_features.rb
@@ -0,0 +1,5 @@
+# 1. Why can't we just replace all calls to total with a local variable? What's the key to watch out for when using a local variable to cache expensive calculations?
+
+# a. The local variable must be outer scoped b. Method will need to have the variable passed in.
+
+# 2. In the first two, play_again? is used in a ternary expression.  (break if play_again? is false)  In the third, (break if play_again? is true)
