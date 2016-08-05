d8d2999377b449efe65fbbadaac98799062eabf6
AWW <walker.albert@gmail.com>
2016-08-05 12:59:17 -0400

    fibonacci pracice - revised

--- lesson_4/fibonacci.rb
+++ lesson_4/fibonacci.rb
@@ -1,22 +1,20 @@
 require 'pry'
 
 numbers = [0, 1]
-num = []
+sequence = []
 def fibonacci(array, new_array)
-  array.each_with_index do |n, idx|
-    if n == array[0] || n == array[1]
-      new_array << n
-    end
+  array.each do |n|
+    new_array << n
   end
   new_array << array[0] += array[1]
-  new_array << (array[1] += array[1])
+  new_array << array[1] += array[1]
   counter = 3
   loop do
-    new_array << (new_array[counter] + new_array[counter - 1])
+    new_array << new_array[counter] + new_array[counter - 1]
     break if new_array.size == 20
     counter += 1
   end
 end
 
-fibonacci(numbers, num)
-p num
+fibonacci(numbers, sequence)
+p sequence
