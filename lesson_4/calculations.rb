b721b4b052a892d2efbdf459f9369ff2c681a870
AWW <walker.albert@gmail.com>
2016-08-05 13:41:49 -0400

    Area of a rectangle in square feet and square meters

---
+++ lesson_4/calculations.rb
@@ -0,0 +1,8 @@
+# Area of a rectangle
+
+length = 2
+width = 4
+square_feet = length * width
+square_meters = square_feet * 0.09290304
+puts "#{square_feet} square feet"
+puts "#{square_meters} square meters"
