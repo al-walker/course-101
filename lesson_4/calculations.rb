3098bbc50df6acd9fed29127d6e3da4d859b222e
AWW <walker.albert@gmail.com>
2016-08-05 13:59:41 -0400

    Division with remainder

--- lesson_4/calculations.rb
+++ lesson_4/calculations.rb
@@ -6,3 +6,16 @@ square_feet = length * width
 square_meters = square_feet * 0.09290304
 puts "#{square_feet} square feet"
 puts "#{square_meters} square meters"
+
+# Division with remainders
+
+# Pizza example
+
+slices = 8
+people = 3
+
+puts "#{people} people sharing #{slices} slices."
+puts "Each person gets #{slices / people} slices"
+if slices % people != 0
+  puts "There are #{slices % people} slices left over."
+end
