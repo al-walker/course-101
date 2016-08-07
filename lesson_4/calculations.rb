abebb4be6c8e84dc12284803409c866689551ac5
AWW <walker.albert@gmail.com>
2016-08-07 11:07:53 -0400

    Add currency formatting using sprintf

--- lesson_4/calculations.rb
+++ lesson_4/calculations.rb
@@ -51,7 +51,7 @@ end
 
 subtotal = (item_prices[0] * quantity[0]) + (item_prices[1] * quantity[1]) + (item_prices[2] * quantity[2])
 
-puts "Subtotal: #{sprintf('%.2f', subtotal)}"
+puts "Subtotal: $#{sprintf('%.2f', subtotal)}"
 puts "Tax: $#{sprintf('%.2f', subtotal * TAX)}"
 total = subtotal + (subtotal * TAX)
 puts "Total: $#{sprintf('%.2f', total)}"
