b30b97fa12ef89d8881db62543ced2c66cc8a799
AWW <walker.albert@gmail.com>
2016-08-11 13:50:07 -0400

    Annotate detect_result method

--- lesson_4/twenty_one_possible_solution_annotated.rb
+++ lesson_4/twenty_one_possible_solution_annotated.rb
@@ -29,7 +29,7 @@ end
 
 # Define a method with one parameter, cards.
 def total(cards)
-# The cards argument points to the first two cards dealt to the players in an array of arrays.
+# The cards argument points to the first two cards dealt to the players in an array of arrays. The array is assigned to the cards variable.
   # The .map method iterates through the cards array and maps the values of each card to a new array. The values are accesible by their index, card[1]
   # The cards array is unaffected since .map is non-destructive.
   values = cards.map { |card| card[1] }
@@ -56,18 +56,29 @@ def total(cards)
     # If there are no aces, .select returns an empty array. Calling .count on an empty array will return 0, sot the code in the block will not run.
     sum -= 10 if sum > HAND_TOTAL
   end
-
+# The total method returns the value of the last expression which is the value pointed to by sum.
   sum
 end
 
+# Define the busted? with one parameter. Include the ? in the method name to indicate that the method will return a boolean value.
 def busted?(cards)
+  # The argument passed to the method is assigned to the cards variable.
+  # The total method returns the sum of the cards in the array passed to the method.
+  # If the conditional statement evaluates to true, the busted? method returns true.
+  # Otherwise the busted? method returns false.
   total(cards) > HAND_TOTAL
 end
 
+# The detect_result method takes two parameters.
 def detect_result(d_cards, p_cards)
+  # The totals of the arguments are assigned to the player_total and dealer_total variables.
+  # d_cards points to an array (all cards in the dealer's hand) of arrays (each containing a cards suit and value).
+  # p_cards points to an array (all cards in the player's hand) of arrays (each containing a cards suit and value).
+  # The sum of the cards in the player's hand is assigned to the player_total
+  # The sum of the cards in the dealer's hand is assigned to the dealer_total
   player_total = total(p_cards)
   dealer_total = total(d_cards)
-
+  # The conditional expression compares totals and returns a symbol to idenitfy the results.
   if player_total > HAND_TOTAL
     :player_busted
   elsif dealer_total > HAND_TOTAL
