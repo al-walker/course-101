6bd2e5dea9ec33f16f5f28c230e2625e642e5881
AWW <walker.albert@gmail.com>
2016-08-17 17:02:08 -0400

    Add scanner

--- lesson_4/random_response.rb
+++ lesson_4/random_response.rb
@@ -1,3 +1,13 @@
+require "strscan"
+
+scanner = StringScanner.new("There are many new characters in the new book.")
+
+p scanner.pos
+
+p scanner.scan(/There/)
+
+p scanner.pos
+
 response = ['yes', 'no', 'maybe']
 
 select = rand(0..2)
