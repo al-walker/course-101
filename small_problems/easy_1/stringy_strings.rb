823ff9bc8744c34d51c1e543217287ebf11f618f
AWW <walker.albert@gmail.com>
2016-08-19 13:10:50 -0400

    Modify after Rubocop

--- small_problems/easy_1/stringy_strings.rb
+++ small_problems/easy_1/stringy_strings.rb
@@ -1,9 +1,12 @@
 
-def stringy(int)
+def stringy(int, start = 1)
   digits = []
   int.times do |d|
-    d.even? ? d = '1' : d = '0'
-    digits << d
+    digits << if start == 1
+                d.even? ? '1' : '0'
+              else
+                d.even? ? '0' : '1'
+              end
   end
   digits.join
 end
@@ -12,3 +15,6 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+puts stringy(2, 0)
+puts stringy(4, 0)
