e0b6b4fe1e6217e37051b33b608371c2c76d39ab
AWW <walker.albert@gmail.com>
2016-08-19 13:32:49 -0400

    Add capability to average floats

--- small_problems/easy_1/array_average.rb
+++ small_problems/easy_1/array_average.rb
@@ -5,8 +5,9 @@ def average(array)
   array.each do |int|
     average << int
   end
-  average.inject(:+) / array.size
+  (average.inject(:+) / array.size).to_f
 end
 
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([2.5, 2.5, 2.5, 2.5]) == 2.5
