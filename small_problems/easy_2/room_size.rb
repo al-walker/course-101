92b2523ec6fa81bed06fac74695a873963dcf965
AWW <walker.albert@gmail.com>
2016-08-24 20:44:11 -0400

    add square feet conversion

--- small_problems/easy_2/room_size.rb
+++ small_problems/easy_2/room_size.rb
@@ -3,8 +3,10 @@ def room_size(length, width)
 end
 
 puts "Enter the length of the room in meters."
-room_length = gets.chomp.to_i
+room_length = gets.chomp.to_f
 puts "Enter the width of the room in meters."
-room_width = gets.chomp.to_i
+room_width = gets.chomp.to_f
 
-p room_size(room_length, room_width)
+meters = room_size(room_length, room_width)
+square_feet = meters * 10.7639
+puts "The area of the room is #{meters} square meters (#{square_feet} square feet)."
