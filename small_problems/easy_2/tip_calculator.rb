acf5aadf4c2e82d628fcaa1a207dcff4344c9935
AWW <walker.albert@gmail.com>
2016-08-26 17:45:12 -0400

    Revise tip caluculator

--- small_problems/easy_2/tip_calculator.rb
+++ small_problems/easy_2/tip_calculator.rb
@@ -1,10 +1,13 @@
+require 'pry'
+
 puts "What is the bill?"
-bill = gets.chomp.to_i
+bill = gets.chomp.to_f
+
 puts "What is the tip percentage?"
 percentage = gets.chomp.to_f
 
-tip = bill * percentage
-total = tip + bill
-
+tip = (bill * (percentage / 100)).round(2)
+total = (tip + bill).round(2)
+binding.pry
 puts "The tip is #{tip}"
 puts "The total is #{total}"
