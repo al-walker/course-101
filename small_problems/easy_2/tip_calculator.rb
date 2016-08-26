67d6f566a7a47be3f935887eab79458710a22a12
AWW <walker.albert@gmail.com>
2016-08-26 17:54:47 -0400

    Add Currency format

--- small_problems/easy_2/tip_calculator.rb
+++ small_problems/easy_2/tip_calculator.rb
@@ -1,26 +1,26 @@
-# require 'pry'
+require 'pry'
+
+puts "What is the bill?"
+bill = gets.chomp.to_f
+
+puts "What is the tip percentage?"
+percentage = gets.chomp.to_f
+
+tip = (bill * (percentage / 100)).round(2)
+total = (tip + bill).round(2)
+
+puts "The tip is #{format("$%.2f", tip)}"
+puts "The total is #{format("$%.2f", total)}"
+
+# puts 'What is the bill?'
+# bill = gets.chomp.to_i
 #
-# puts "What is the bill?"
-# bill = gets.chomp.to_f
+# convert = '.01'
 #
-# puts "What is the tip percentage?"
-# percentage = gets.chomp.to_f
-#
-# tip = (bill * (percentage / 100)).round(2)
-# total = (tip + bill).round(2)
+# puts 'What is the tip percentage?'
+# percentage = gets.chomp.to_i
+# multiplier = convert.to_f
+# tip = bill * percentage * multiplier
 #
 # puts "The tip is $#{tip}"
-# puts "The total is $#{total}"
-
-puts 'What is the bill?'
-bill = gets.chomp.to_i
-
-convert = '.01'
-
-puts 'What is the tip percentage?'
-percentage = gets.chomp.to_i
-multiplier = convert.to_f
-tip = bill * percentage * multiplier
-
-puts "The tip is $#{tip}"
-puts "The total is $#{bill + tip}"
+# puts "The total is $#{bill + tip}"
