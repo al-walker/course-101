dcf80fd3a9aefcdd9e825e4537a9462c97e9408c
AWW <walker.albert@gmail.com>
2016-08-26 17:55:54 -0400

    Add Currency format - remove additional solution

--- small_problems/easy_2/tip_calculator.rb
+++ small_problems/easy_2/tip_calculator.rb
@@ -11,16 +11,3 @@ total = (tip + bill).round(2)
 
 puts "The tip is #{format("$%.2f", tip)}"
 puts "The total is #{format("$%.2f", total)}"
-
-# puts 'What is the bill?'
-# bill = gets.chomp.to_i
-#
-# convert = '.01'
-#
-# puts 'What is the tip percentage?'
-# percentage = gets.chomp.to_i
-# multiplier = convert.to_f
-# tip = bill * percentage * multiplier
-#
-# puts "The tip is $#{tip}"
-# puts "The total is $#{bill + tip}"
