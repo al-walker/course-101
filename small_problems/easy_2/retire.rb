43e03bb14dce029c669a0fb40e62a211194b652e
AWW <walker.albert@gmail.com>
2016-08-27 10:58:04 -0400

    Run Rubocop

--- small_problems/easy_2/retire.rb
+++ small_problems/easy_2/retire.rb
@@ -4,7 +4,6 @@ age = gets.chomp.to_i
 puts "At what age would you like to retire?"
 retire_age = gets.chomp.to_i
 
-
-
-puts "It's #{Time.now.year}. You will retire in #{Time.now.year + (retire_age - age)}."
+puts "It's #{Time.now.year}.
+You will retire in #{Time.now.year + (retire_age - age)}."
 puts "You have only #{retire_age - age} years of work to go!"
