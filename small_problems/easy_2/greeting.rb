45b650000efa1d95a892f9b3a40ff19c3b872c63
AWW <walker.albert@gmail.com>
2016-08-27 11:44:41 -0400

    Add regex

--- small_problems/easy_2/greeting.rb
+++ small_problems/easy_2/greeting.rb
@@ -3,8 +3,10 @@ require 'pry'
 print "What is your name?"
 name = gets.chomp
 
-if name.scan /\w+!/
-puts 'why are we screaming?'.upcase
-else
-puts "Hello, #{name}."
-end
+match = name.scan /\w+!/
+
+if match
+  p 'why are we screaming?'.upcase
+  else
+  puts "Hello, #{name}."
+  end
