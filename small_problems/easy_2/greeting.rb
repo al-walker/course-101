365542aeed185dde97529561e06b27607c1ce0f2
AWW <walker.albert@gmail.com>
2016-08-28 08:46:03 -0400

    Add ternary expression

--- small_problems/easy_2/greeting.rb
+++ small_problems/easy_2/greeting.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 print "What is your name?"
 name = gets.chomp
-if name.chars.last == '!'
-  p 'why are we screaming?'.upcase
-else
-  puts "Hello, #{name}."
-#p match.to_s == name ? screaming : greet
-end
+
+# if name.chars.last == '!'
+#   p 'why are we screaming?'.upcase
+# else
+#   puts "Hello, #{name}."
+# end
+
+p name.chars.last == '!' ? 'why are we screaming?'.upcase : "Hello, #{name}."
