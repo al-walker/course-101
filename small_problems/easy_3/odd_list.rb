fa5e8fa51cd0af170c03a9e3124842480bcfab87
AWW <walker.albert@gmail.com>
2016-09-03 09:02:02 -0400

    Add companion method - even_idx

--- small_problems/easy_3/odd_list.rb
+++ small_problems/easy_3/odd_list.rb
@@ -8,7 +8,26 @@ def oddities(list)
   odd
 end
 
-p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-p oddities(['abc', 'def']) == ['abc']
-p oddities([123]) == [123]
-p oddities([]) == []
+def even_idx(list)
+  even = []
+  list.each_with_index do |item, idx|
+    even << item unless idx == 0 || idx.odd?
+  end
+  even
+end
+
+# p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+# p oddities(['abc', 'def']) == ['abc']
+# p oddities([123]) == [123]
+# p oddities([]) == []
+
+# p even_idx([2, 3, 4, 5, 6]) == [2, 4, 6]
+# p even_idx(['abc', 'def']) == ['abc']
+# p even_idx([123]) == [123]
+# p even_idx([]) == []
+
+p even_idx([2, 3, 4, 5, 6]) == [4, 6]
+p even_idx(['abc', 'def']) == []
+p even_idx([123]) == []
+p even_idx(['abc', 'def', 'ghi']) == ['ghi']
+p even_idx([]) == []
