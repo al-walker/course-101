5e7796abeb8bbbd28f5a753ef7b042794ba4d95b
AWW <walker.albert@gmail.com>
2016-09-03 08:06:28 -0400

    Add xor.rb

---
+++ small_problems/easy_3/xor.rb
@@ -0,0 +1,16 @@
+def xor?(arg1, arg2)
+  if arg1 == false && arg2 == false
+    false
+  elsif arg1 == true && arg2 == true
+    false
+  else
+    arg1 == true && arg2 == false || arg1 == false && arg2 == true
+    true
+  end
+end
+
+
+
+p xor?(5.odd?, 4.odd?) # == true
+p xor?(5.odd?, 4.even?) # == false
+p xor?(5.even?, 4.odd?) # == false
