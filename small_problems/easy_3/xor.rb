4bdb16684f8efe79947826673e8480ec60916c4c
AWW <walker.albert@gmail.com>
2016-09-03 08:10:19 -0400

    Add ternary xor

--- small_problems/easy_3/xor.rb
+++ small_problems/easy_3/xor.rb
@@ -1,13 +1,17 @@
 def xor?(arg1, arg2)
-  if arg1 == false && arg2 == false
-    false
-  elsif arg1 == true && arg2 == true
-    false
-  else
-    arg1 == true && arg2 == false || arg1 == false && arg2 == true
-    true
-  end
+   arg1 && !arg2 || arg2 && !arg1 ? true : false
 end
+
+# def xor?(arg1, arg2)
+#   if arg1 == false && arg2 == false
+#     false
+#   elsif arg1 == true && arg2 == true
+#     false
+#   else
+#     arg1 == true && arg2 == false || arg1 == false && arg2 == true
+#     true
+#   end
+# end
 
 
 
