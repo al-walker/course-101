2bafbbeb28baff59fe303ef980ea4a8fa0b4d183
AWW <walker.albert@gmail.com>
2016-09-04 09:02:38 -0400

    Add palindromic_num.rb

---
+++ small_problems/easy_3/palindromic_num.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+def palindromic_number?(num)
+  num.to_s.chars == num.to_s.chars.reverse
+end
+
+
+p palindromic_number?(34543) # == true
+p palindromic_number?(123210) # == false
+p palindromic_number?(22) # == true
+p palindromic_number?(5) # == true
