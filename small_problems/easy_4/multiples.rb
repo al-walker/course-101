45e477b1ecf39ddc02e9811011795366350372d7
AWW <walker.albert@gmail.com>
2016-09-05 18:05:30 -0400

    Add multiples.rb

---
+++ small_problems/easy_4/multiples.rb
@@ -0,0 +1,16 @@
+require 'pry'
+def multisum(num)
+  multiples = []
+  (1..num).to_a.each do |n|
+    if n % 3 == 0 || n % 5 == 0
+    multiples << n
+    end
+  end
+  multiples.reduce(:+)
+end
+
+
+p multisum(3) # == 3
+p multisum(5) # == 8
+p multisum(10) # == 33
+p multisum(1000) # == 234168
