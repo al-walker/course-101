b0308871058ae3c6b07362038b80bc65945c68fc
AWW <walker.albert@gmail.com>
2016-09-10 12:32:48 -0400

    Rubocop fizzbuzz.rb

--- small_problems/easy_4/fizzbuzz.rb
+++ small_problems/easy_4/fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(num1, num2)
   fizzbuzz_array = []
   (num1..num2).each do |n|
-  if n % 3 == 0 && n % 5 == 0
-    fizzbuzz_array << 'fizzbuzz'
-  elsif n % 3 == 0
-    fizzbuzz_array << 'fizz'
-  elsif n % 5 == 0
-    fizzbuzz_array << 'buzz'
-  else
-    fizzbuzz_array << n
+    if n % 3 == 0 && n % 5 == 0
+      fizzbuzz_array << 'fizzbuzz'
+    elsif n % 3 == 0
+      fizzbuzz_array << 'fizz'
+    elsif n % 5 == 0
+      fizzbuzz_array << 'buzz'
+    else
+      fizzbuzz_array << n
+    end
   end
- end
- fizzbuzz_array
+  fizzbuzz_array
 end
 
 p fizzbuzz(1, 15)
