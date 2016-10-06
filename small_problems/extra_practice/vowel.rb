05bbb8ea4ec08d3fe5448ae222cb7d4da8e3f938
AWW <walker.albert@gmail.com>
2016-10-05 20:42:36 -0400

    Refactor vowel.rb

--- small_problems/extra_practice/vowel.rb
+++ small_problems/extra_practice/vowel.rb
@@ -6,9 +6,9 @@ def remove_vowels(str)
   vowels = %w(a e i o u)
   new = []
   str.each do |w|
-    w.split.select do |l|
-      new << l.chars.select { |letter| !vowels.include?(letter) }
-    end
+    # w.split.select do |l|
+      new << w.chars.select { |letter| !vowels.include?(letter) }
+    # end
   end
   new.each { |no_vowel| p no_vowel.join }
 end
