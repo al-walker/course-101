6534237a1faa75b736515bfcc314a03794a2bc04
AWW <walker.albert@gmail.com>
2016-10-21 18:27:59 -0400

    Add digits.rb

---
+++ small_problems/easy_6/easy_1/digits.rb
@@ -0,0 +1,17 @@
+require 'pry'
+require 'rubocop'
+# Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
+
+# given an integer
+# covert to string
+# split the string
+# return the digits as an array of ints
+
+def digit_list(int)
+  int.to_s.chars.map(&:to_i)
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]
+puts digit_list(7) == [7]
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0]
+puts digit_list(444) == [4, 4, 4]
