bf88d93ca6586c56f2ec775952fbd67da50f1cbd
AWW <walker.albert@gmail.com>
2016-10-24 18:53:43 -0400

    Add teddy. rb

---
+++ small_problems/easy_6/easy_1/teddy.rb
@@ -0,0 +1,18 @@
+# Build a program that randomly generates and prints Teddy's age.
+# To get the age, you should generate a random number between 20 and 200.
+
+# input
+#
+# processes
+# generate random number between 20 and 200
+# Display teddy's age
+#
+# output
+# teddy's age
+
+def teddys_age
+  puts "Teddy is #{rand(20..200)} years old!"
+end
+
+teddys_age
+# Teddy is 69 years old!
