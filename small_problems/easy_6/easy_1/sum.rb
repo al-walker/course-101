9060dea4c7c0f030d3a25cabfdc3d49a5d553808
AWW <walker.albert@gmail.com>
2016-10-24 18:24:48 -0400

    Add sum.rb

---
+++ small_problems/easy_6/easy_1/sum.rb
@@ -0,0 +1,27 @@
+require 'pry'
+require 'rubocop'
+
+# Write a method that takes one argument, a positive integer, and returns the sum of its digits.
+
+# input - positive integer
+#
+# variables
+# int
+# integer_string
+# array_of_strings
+#
+# processes
+# -convert int to string
+# -split the string into chars
+# -convert each char into an integer
+# -add the integers
+#
+# output - sum of positive integers digits
+
+def sum(int)
+  int.to_s.chars.map { |s| s.to_i }.reduce(:+)
+end
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
