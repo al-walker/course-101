3e7f7cceb8bc4f8ca94ca744eec57647daf0aacf
AWW <walker.albert@gmail.com>
2016-10-26 14:54:13 -0400

    Rubocop retire.rb

--- small_problems/easy_6/easy_1/retire.rb
+++ small_problems/easy_6/easy_1/retire.rb
@@ -32,7 +32,6 @@ def retirement_calculator(current_year)
 
   puts "It's #{current_year}. You will retire in #{retirement_year}."
   puts "You have only #{retirement_year - current_year} years of work to go!"
-
 end
 
 retirement_calculator(2016)
