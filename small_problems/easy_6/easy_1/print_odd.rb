ead5477d16133359f767630f710c60db53e7d63c
AWW <walker.albert@gmail.com>
2016-10-26 17:01:28 -0400

    Add print_odd.rb

---
+++ small_problems/easy_6/easy_1/print_odd.rb
@@ -0,0 +1,31 @@
+# require 'pry'
+# require 'rubocop'
+#
+# # Print all odd numbers from 1 to 99, inclusive.
+# # All numbers should be printed on separate lines.
+#
+# input
+# range 1..99
+#
+# processes
+# select odd numbers
+# print odd numbers on separate lines
+# output
+# odd numbers on separate lines
+#
+# pseudocode
+odd = []
+(1..99).map { |n| odd << n if n % 2 != 0 }
+odd.each { |n| p n}
+# iterate through
+# check if each number is odd
+# collect odd numbers into new array
+# loop through new array and print all the numbers
+#
+# tests
+# 1
+# 3
+# 5
+# 7
+# 9
+# 11
