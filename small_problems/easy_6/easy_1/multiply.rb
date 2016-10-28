bddb6906ee65ab4db3fafea530bd53731053447a
AWW <walker.albert@gmail.com>
2016-10-28 17:49:22 -0400

    Add multiply.rb

---
+++ small_problems/easy_6/easy_1/multiply.rb
@@ -0,0 +1,22 @@
+require 'pry'
+require 'rubocop'
+
+# Create a method that takes two arguments,
+# multiplies them together, and returns the result.
+
+# input
+# number_1
+# number_2
+# def multiply(number_1, number_2)
+#
+# processes
+# product = number_1 * number_2
+#
+# output
+# product
+
+def multiply(number_1, number_2)
+  number_1 * number_2
+end
+
+p multiply(5, 3) # == 15
