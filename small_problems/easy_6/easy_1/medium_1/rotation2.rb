f6a0b9fec6c79c7caef10697a8f19248000968de
AWW <walker.albert@gmail.com>
2016-11-05 15:49:14 -0400

    Rubocop rotation2.rb

--- small_problems/easy_6/easy_1/medium_1/rotation2.rb
+++ small_problems/easy_6/easy_1/medium_1/rotation2.rb
@@ -15,9 +15,8 @@ def rotate_rightmost_digits(number, rotation_digit)
   new_number = [] # hold new digit order; join; to_i
   counter = 0 # loop through digits as array of strings
   loop do
-    # binding.pry
     # loop through number add all but string at rotation_digit (as number from end)
-    unless number.to_s.chars[-(rotation_digit)].to_i == number.to_s.chars[counter].to_i
+    unless number.to_s.chars[-rotation_digit].to_i == number.to_s.chars[counter].to_i
       new_number << number.to_s.chars[counter].to_i
     end
     counter += 1
@@ -25,7 +24,6 @@ def rotate_rightmost_digits(number, rotation_digit)
   end
   number.to_s.chars.select { |n| new_number << n.to_i if n == number.to_s.chars[-rotation_digit] }
   new_number.join.to_i
-  binding.pry
 end
 
 p rotate_rightmost_digits(735291, 1) == 735291
