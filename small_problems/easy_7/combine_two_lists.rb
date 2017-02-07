d60612e67dc875a729bb0ed3d611fb4c6cf72568
AWW <walker.albert@gmail.com>
2017-02-06 20:30:14 -0500

    rubocop interleave

--- small_problems/easy_7/combine_two_lists.rb
+++ small_problems/easy_7/combine_two_lists.rb
@@ -1,3 +1,5 @@
+require 'rubocop'
+
 # input: two arrays passed in as arguments to a method
 # output: new array all elements alternating
 # algrithm: .map.zip
@@ -6,4 +8,4 @@ def interleave(arr1, arr2)
   arr1.zip(arr2).flatten
 end
 
-p interleave([1, 2, 3], ['a', 'b', 'c']) # == [1, 'a', 2, 'b', 3, 'c']
+p interleave([1, 2, 3], %w(a b c)) # == [1, 'a', 2, 'b', 3, 'c']
